45060a6c09422be2c358b6a9f49b12f64b20fc3e
A.J. Marchese <aj.marchese@outlook.com>
2023-05-31 08:44:29 -0700

    Add project files.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Q to quit the program.");
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Q)
+                {
+                    Console.WriteLine("You pressed Q. The program will now quit.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You pressed " + keyInfo.Key + ". Press Q to quit.");
+                }
+            }
+            Console.Write("Enter a number: ");  // Prompt the user
+            string? name = Console.ReadLine();
+
+            Addition(1, 2, 3, 3, 4);
+
+        }
+
+        public static void Addition(params float[] numbers)
+        {
+            float sum = 0;
+
+            foreach (float number in numbers)
+            {
+                sum += number;
+            }
+            Display(sum);
+
+        }
+
+        public static float Display(float result) => result;
+    }
+}
