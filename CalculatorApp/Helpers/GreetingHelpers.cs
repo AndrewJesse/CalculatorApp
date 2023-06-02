62df1fc3a9992f51fb50c83de5d3bd3c0b36764b
AndrewJesse <aj.marchese@outlook.com>
2023-06-02 10:45:40 -0700

    moved greeting to helpers folder

---
+++ CalculatorApp/Helpers/GreetingHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Helpers
+{
+    internal class GreetingHelpers
+    {
+        public static void Greeting()
+        {
+            Console.WriteLine("Welcome to the Command Line Calculator\n");
+            Console.WriteLine("https://github.com/AndrewJesse/CalculatorApp\n");
+            Console.WriteLine(" _____________________");
+            Console.WriteLine("|  _________________  |");
+            Console.WriteLine("| |            80085| |");
+            Console.WriteLine("| |_________________| |");
+            Console.WriteLine("|  ___ ___ ___   ___  |");
+            Console.WriteLine("| | 7 | 8 | 9 | | + | |");
+            Console.WriteLine("| |___|___|___| |___| |");
+            Console.WriteLine("| | 4 | 5 | 6 | | - | |");
+            Console.WriteLine("| |___|___|___| |___| |");
+            Console.WriteLine("| | 1 | 2 | 3 | | x | |");
+            Console.WriteLine("| |___|___|___| |___| |");
+            Console.WriteLine("| | . | 0 | = | | / | |");
+            Console.WriteLine("| |___|___|___| |___| |");
+            Console.WriteLine("|_____________________|\n");
+        }
+    }
+}
--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using MathNet.Symbolics;
+using CalculatorApp.Helpers;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 
 namespace CalculatorApp
@@ -10,7 +11,7 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            Greeting();
+            GreetingHelpers.Greeting();
             Run();
         }
         public static void Run()
@@ -35,24 +36,6 @@ namespace CalculatorApp
 
             } while (userInput != 'N' && userInput != 'n');
         }
-        public static void Greeting()
-        {
-            Console.WriteLine("Welcome to the Command Line Calculator\n");
-            Console.WriteLine("https://github.com/AndrewJesse/CalculatorApp\n");
-            Console.WriteLine(" _____________________");
-            Console.WriteLine("|  _________________  |");
-            Console.WriteLine("| |            80085| |");
-            Console.WriteLine("| |_________________| |");
-            Console.WriteLine("|  ___ ___ ___   ___  |");
-            Console.WriteLine("| | 7 | 8 | 9 | | + | |");
-            Console.WriteLine("| |___|___|___| |___| |");
-            Console.WriteLine("| | 4 | 5 | 6 | | - | |");
-            Console.WriteLine("| |___|___|___| |___| |");
-            Console.WriteLine("| | 1 | 2 | 3 | | x | |");
-            Console.WriteLine("| |___|___|___| |___| |");
-            Console.WriteLine("| | . | 0 | = | | / | |");
-            Console.WriteLine("| |___|___|___| |___| |");
-            Console.WriteLine("|_____________________|\n");
-        }
+
     }
 }
